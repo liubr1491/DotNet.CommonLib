342c74515b8242a413b1fab00007b310ed50f5a9
liubr <liubr@ctrip.com>
2014-11-04 09:08:04 +0800

    增加控制台项目

---
+++ DotNet.CommonLib.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DotNet.CommonLib.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                for (int i = 0; i < 9600000; i++)
+                {
+
+                }
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
